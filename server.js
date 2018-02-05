5fc63ec3b792e497820068b93fd73f765b29a3bd
dankato <apearmor@gmail.com>
2018-02-04 23:16:03 -0800

    mongo connected

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb');
 
 const port = 3000;
 const app = express();
+const dburl = 'dburl-goes-here';
+
 
 // extract data from <form> into the body property in the req object
 app.use(bodyParser.urlencoded({extended: true}));
+
+// connect to server
+MongoClient.connect(dburl, (error, client) => {
+    let db;
+    // start the server
+    if(error) return console.log(error)
+    db = client.db('crud-quotes')
+
+    app.listen(port, function() {
+        console.log(`listening on port ${port}.`);
+    });
+});
+
 
 // READ (path, callback)
 app.get('/', (req, res) => {
@@ -16,8 +32,4 @@ app.get('/', (req, res) => {
 // CREATE
 app.post('/quotes', (req, res) => {
     console.log(req.body);
-});
-
-app.listen(port, function() {
-    console.log(`listening on port ${port}.`);
 });
